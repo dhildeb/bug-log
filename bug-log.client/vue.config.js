a43526ddca0076a7af8e421b5a3e70f193489419
daniel hildebrandt <d.hildebrandt93@gmail.com>
2021-08-03 16:56:15 -0600

    again

--- bug-log.client/vue.config.js
+++ bug-log.client/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // NOTE to host on github publicPath: '/YOUR-GITHUB-REPOSITORY-NAME/',
   publicPath: '/bug-log',
-  outputDir: '../bug-log.server',
+  outputDir: '../bug-log.server/client',
   productionSourceMap: false
 }
